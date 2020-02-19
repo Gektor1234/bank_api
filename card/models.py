ec2d44cca93b23ab3f7434be99f87368b965abb9
Roman <xyvafvyf1@gmail.com>
2020-02-19 19:04:03 +0300

    only positive value

--- card/models.py
+++ card/models.py
@@ -8,13 +8,20 @@ class Man(models.Model):
         return self.name
 
 class Card(models.Model):
-    number = models.IntegerField() # номер карты
-    balance = models.IntegerField() # баланс
+    number = models.PositiveIntegerField(default=0) # номер карты
+    balance = models.PositiveIntegerField(default=0) # баланс
     man = models.ForeignKey('Man', related_name='cards', on_delete=models.CASCADE)   # держатель карты
 
+    class Meta:
+        managed = True
+        db_table = 'card'
+        constraints = [
+            models.CheckConstraint(check=models.Q(balance__gte='0'), name='card_balance_non_negative'),
+        ]
+
 class Transaction(models.Model):
-    number = models.IntegerField()
-    balance = models.IntegerField()
+    number = models.PositiveIntegerField(default=0)
+    balance = models.PositiveIntegerField(default=0)
     man = models.ForeignKey('Man', related_name='transaction', on_delete=models.CASCADE)
 
 
